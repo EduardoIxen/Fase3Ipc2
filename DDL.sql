48c047988f1fcce2885de07ae8bef9de0d0feaae
EduardoIxen <tomas-1539@hotmail.com>
2021-01-02 10:45:57 -0600

    Script de la base de datos

--- DDL.sql
+++ DDL.sql
@@ -117,6 +117,9 @@ create table Prestamo(
     modalidadAPagar numeric(2,0) not null,
     codigoCliente bigint,
     idEmpresa bigint,
+    descripcion varchar(100),
+    cuenta bigint,
+    aprobado boolean,
     foreign key(codigoCliente) references ClienteIndividual(cui),
     foreign key(idEmpresa) references Empresa(idEmpresa)
 );
@@ -169,3 +172,33 @@ insert into TipoDeCliente(nombre) values('Cliente Individual');
 insert into TipoDeCliente(nombre) values('Empresa');
 insert into Marca(nombre) values('PREFEPUNTOS');
 insert into Marca(nombre) values('CASHBACK');
+insert into ClienteIndividual values(3130, 3345461, 'DIEGO FERNANDO CORTEZ LOPEZ', '1990-02-12', 'diegof', 'diegof');
+insert into ClienteIndividual values(3131, 9345462, 'KARINA NOHEMI RAMIREZ ORELLANA', '1990-02-12', 'karinan', 'karinan');
+insert into ClienteIndividual values(3132, 8345463, 'ANGEL GEOVANY ARAGON PEREZ', '1990-02-12', 'angelg', 'angelg');
+insert into ClienteIndividual values(3133, 7345464, 'CARLOS ROBERTO QUIXTAN PEREZ', '1990-02-12', 'carlosr', 'carlosr');
+insert into ClienteIndividual values(3134, 6345465, 'ERICK IVAN MAYORGA RODRIGUEZ', '1990-02-12', 'ericki', 'ericki');
+insert into ClienteIndividual values(3135, 5345466, 'BYRON ESTUARDO CAAL CATUN', '1990-02-12', 'byrone', 'byrone');
+insert into ClienteIndividual values(3136, 4345467, 'RONALD RODRIGO MARIN SALAS', '1990-02-12', 'ronaldr', 'ronaldr');
+insert into ClienteIndividual values(3137, 3345468, 'OSCAR DANIEL OLIVA', '1990-02-12', 'oscard', 'oscard');
+insert into ClienteIndividual values(3138, 2345469, 'EDUARDO ABRAHAM BARILLAS', '1990-02-12', 'eduardoa', 'eduardoa');
+insert into ClienteIndividual values(3139, 1345460, 'CARLOS ESTUARDO MONTERROSO SANTOS', '1990-02-12', 'carlose', 'carlose');
+insert into CuentaMonetaria values(541234, 1, 15.00, 0.00);
+insert into CuentaMonetaria values(263769, 1, 15.00, 0.00);
+insert into CuentaMonetaria values(481366, 1, 15.00, 0.00);
+insert into CuentaMonetaria values(152352, 1, 15.00, 0.00);
+insert into CuentaMonetaria values(358054, 1, 15.00, 0.00);
+insert into CuentaMonetaria values(503944, 1, 15.00, 0.00);
+insert into CuentaMonetaria values(316167, 1, 15.00, 0.00);
+insert into CuentaMonetaria values(374296, 1, 15.00, 0.00);
+insert into CuentaMonetaria values(556658, 1, 15.00, 0.00);
+insert into CuentaMonetaria values(462978, 1, 15.00, 0.00);
+insert into DetalleClienteCuenta(codigoCliente, codigoCuentaMonetaria, estaActiva) values(3130,541234 ,True);
+insert into DetalleClienteCuenta(codigoCliente, codigoCuentaMonetaria, estaActiva) values(3131,263769,True);
+insert into DetalleClienteCuenta(codigoCliente, codigoCuentaMonetaria, estaActiva) values(3132,481366,True);
+insert into DetalleClienteCuenta(codigoCliente, codigoCuentaMonetaria, estaActiva) values(3133,152352,True);
+insert into DetalleClienteCuenta(codigoCliente, codigoCuentaMonetaria, estaActiva) values(3134,358054,True);
+insert into DetalleClienteCuenta(codigoCliente, codigoCuentaMonetaria, estaActiva) values(3135,503944,True);
+insert into DetalleClienteCuenta(codigoCliente, codigoCuentaMonetaria, estaActiva) values(3136,316167,True);
+insert into DetalleClienteCuenta(codigoCliente, codigoCuentaMonetaria, estaActiva) values(3137,374296,True);
+insert into DetalleClienteCuenta(codigoCliente, codigoCuentaMonetaria, estaActiva) values(3138,556658,True);
+insert into DetalleClienteCuenta(codigoCliente, codigoCuentaMonetaria, estaActiva) values(3139,462978,True);
